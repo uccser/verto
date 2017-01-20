90dbfc0a5c334c437e8b3edf02c2854d67a7ae49
Jack Morgan <jackmorgannz@gmail.com>
2017-01-20 22:29:36 +1300

    Implement base test case class to avoid repeated logic
    The base test case class provides the
    following:
    - A read file function that uses the correct
      encoding (#18).
    - A variable to set all the difference amount
      for all tests at once.
    
    Classes using this base class need to call the
    parent's initialiser and also set the tag_name
    variable. This is used to identify the folder
    of test case files.
    
    The base test case class could also be set to
    be abstract, to prevent a plain base test case
    being created.

--- tests/start_tests.py
+++ tests/start_tests.py
@@ -11,35 +11,52 @@ from tests.image_tests import *
 from tests.video_tests import *
 from tests.interactive_tests import *
 
-
 class BaseTestCase(unittest.TestCase):
-    """
-    Base test
-    """
-    # could create the md instance here?
-    pass
+    """A base test class for individual test classes"""
+    
+    def __init__(self, *args, **kwargs):
+        """Creates BaseTest Case class
+        
+        Create class inheiriting from TestCase, while also storing
+        the path to test files and the maxiumum difference to display on
+        test failures.
+        """
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        self.test_file_path = 'tests/assets/{tag_name}/{filename}.txt'
+        self.maxDiff = 640  # Set to None for full output of all test failures
+
+    def read_test_file(self, filename):
+        """Returns a string for a given file
+        
+        This function reads a file from a given filename in UTF-8 encoding.
+        """
+        file_path = self.test_file_path.format(tag_name=self.tag_name, filename=filename)
+        file_object = open(file_path, encoding="utf-8")
+        return file_object.read()
+        
+    def setUp(self):
+        self.md = markdown.Markdown(extensions=[CSFGExtension()])
+
+    def tearDown(self):
+        self.md = None
 
 
 def suite():
     # NTS what order should these go in?
     allSuites = unittest.TestSuite((
-        unittest.makeSuite(GlossaryLinkTest, 'test'), # order of tests by cmp()
-        unittest.makeSuite(PanelTest, 'test'),
-        unittest.makeSuite(CommentTest, 'test'),
-        unittest.makeSuite(HeadingTest, 'test'),
-        unittest.makeSuite(ImageTest, 'test'),
-        unittest.makeSuite(VideoTest, 'test'),
-        unittest.makeSuite(InteractiveTest, 'test')
-        ))
+        unittest.makeSuite(GlossaryLinkTest), # order of tests by cmp()
+        unittest.makeSuite(PanelTest),
+        unittest.makeSuite(CommentTest),
+        unittest.makeSuite(HeadingTest),
+        unittest.makeSuite(ImageTest),
+        unittest.makeSuite(VideoTest),
+        unittest.makeSuite(InteractiveTest)
+    ))
 
     return allSuites
-
 
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     test_suite = suite()
     runner.run(test_suite)
-
-
-
