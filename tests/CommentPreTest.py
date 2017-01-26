e5121927d7ee7c58d70cf8c015e27e045b27f05f
Hayley van Waas <hvw10@uclive.ac.nz>
2017-01-26 17:24:27 +1300

    Deleting old code

--- tests/CommentPreTest.py
+++ tests/CommentPreTest.py
@@ -114,30 +114,3 @@ class CommentPreTest(BaseTestCase):
         expected_string = self.read_test_file('contains_multiple_inline_comments_expected')
         self.assertEqual(expected_string, converted_test_string)
 
-
-    """
-    def test_match_false(self):
-        test_string = self.read_test_file('fail_string')
-        self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-    def test_match_single_word_term_true(self):
-        test_string = self.read_test_file('basic')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-    def test_parses_basic(self):
-        test_string = self.read_test_file('basic')
-        converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
-        expected_file_string = self.read_test_file('basic_expected')
-        self.assertEqual(converted_test_string, expected_file_string)
-
-    # NTS should preprocessor tests be separate?
-    def test_preprocessor_match(self):
-        test_string = self.read_test_file('singleline')
-        self.assertTrue(CommentPreprocessor(self.ext, self.md.parser).test(test_string), msg='"{}"'.format(test_string))
-
-    def test_preprocessor_parsing(self):
-        test_string = self.read_test_file('singleline')
-        converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
-        expected_file_string = self.read_test_file('singleline_expected')
-        self.assertEqual(converted_test_string, expected_file_string)
-    """
