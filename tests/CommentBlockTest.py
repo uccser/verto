7eeaa1fdbcc7c1b02f53a7879f7d4cba5b6ef792
Hayley van Waas <hayleyavw@gmail.com>
2017-01-30 20:37:01 +1300

    Restructured existing tests to use more logical functions
    Group tests by input file rather than test type. i.e. rather than a
    function tests there is a match for many different input files,
    functions test for a match in a specific file, and then compare the
    converted text to the expected result.

--- tests/CommentBlockTest.py
+++ tests/CommentBlockTest.py
@@ -22,94 +22,81 @@ class CommentBlockTest(BaseTestCase):
         self.ext = Mock()
         self.ext.tag_patterns = BaseTestCase.loadTagPatterns(self)
 
-    def test_block_match_false(self):
-        """
-        Test no match found in string where there is no block comment.
-        """
+    def test_no_block_comment(self):
         test_string = self.read_test_file('no_block_comment')
         self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
 
-        test_string = self.read_test_file('text_contains_the_word_comment')
-        self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_inline_comment')
-        self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-
-    def test_block_match_true(self):
-        """
-        Test finds matches where block comments do exist.
-        """
-        test_string = self.read_test_file('contains_block_comment')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_inline_then_block_comment')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_block_then_inline_comment')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_inline_then_block_then_inline_comment')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_multiple_block_comments')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-        test_string = self.read_test_file('contains_block_comment_on_single_line')
-        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
-
-
-    def test_unchanged(self):
-        """
-        Test text with no matches is unchanged.
-        """
-        test_string = self.read_test_file('no_block_comment')
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('no_block_comment_expected')
         self.assertEqual(expected_string, converted_test_string)
 
-        test_string = self.read_test_file('contains_inline_comment')
-        converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
-        expected_string = self.read_test_file('contains_inline_comment_expected_block')
-        self.assertEqual(expected_string, converted_test_string)
-
+    def test_text_contains_the_word_comment(self):
         test_string = self.read_test_file('text_contains_the_word_comment')
+        self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('text_contains_the_word_comment_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_inline_comment(self):
+        test_string = self.read_test_file('contains_inline_comment')
+        self.assertFalse(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
 
-    def test_inline_removed(self):
-        """
-        Test text matching block comment regex is removed
-        """
+        converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
+        expected_string = self.read_test_file('contains_inline_comment_expected_block')
+        self.assertEqual(expected_string, converted_test_string)
+
+    def test_contains_block_comment(self):
         test_string = self.read_test_file('contains_block_comment')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_block_comment_expected_block')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_inline_then_block_comment(self):
         test_string = self.read_test_file('contains_inline_then_block_comment')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_inline_then_block_comment_expected_block')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_block_then_inline_comment(self):
         test_string = self.read_test_file('contains_block_then_inline_comment')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_block_then_inline_comment_expected_block')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_inline_then_block_then_inline_comment(self):
         test_string = self.read_test_file('contains_inline_then_block_then_inline_comment')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_inline_then_block_then_inline_comment_expected_block')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_multiple_block_comemnts(self):
         test_string = self.read_test_file('contains_multiple_block_comments')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_multiple_block_comments_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_block_comment_on_single_line(self):
         test_string = self.read_test_file('contains_block_comment_on_single_line')
+        self.assertTrue(CommentBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg='"{}"'.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_block_comment_on_single_line_expected_block')
         self.assertEqual(expected_string, converted_test_string)
+
+    def test_block_comment_contains_another_block_comment(self):
+        pass
+
+    def test_block_comment_contains_inline_comment(self):
+        pass
 
--- tests/ImageTest.py
+++ tests/ImageTest.py
@@ -17,67 +17,50 @@ class ImageTest(BaseTestCase):
         self.ext.tag_patterns = BaseTestCase.loadTagPatterns(self)
         self.ext.required_files = defaultdict(set)
 
-
-    def test_match_false(self):
-        """
-        Test no match found in string where there is no image
-        """
+    def test_no_image(self):
         test_string = self.read_test_file('no_image')
         self.assertFalse(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
 
-        test_string = self.read_test_file('text_contains_the_word_image')
-        self.assertFalse(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
-
-    def test_match_true(self):
-        """
-        Test finds matches in strings where there is an image included
-        """
-        test_string = self.read_test_file('contains_image')
-        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
-
-        test_string = self.read_test_file('contains_multiple_images')
-        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
-
-        test_string = self.read_test_file('contains_image_and_text_contains_word_image')
-        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
-
-        test_string = self.read_test_file('image_in_panel')
-        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
-
-    def test_unchanged(self):
-        """
-        Test text with no matches is unchanged
-        """
-        test_string = self.read_test_file('no_image')
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('no_image_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_text_contains_the_word_image(self):
         test_string = self.read_test_file('text_contains_the_word_image')
+        self.assertFalse(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('text_contains_the_word_image_expected')
         self.assertEqual(expected_string, converted_test_string)
 
-    def test_image_link_converted_to_django_tag(self):
-        """
-        Test text with image is converted to django image syntax
-        """
+    def test_contains_image(self):
         test_string = self.read_test_file('contains_image')
+        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_image_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_multiple_images(self):
         test_string = self.read_test_file('contains_multiple_images')
+        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_multiple_images_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_image_and_text_contains_word_image(self):
         test_string = self.read_test_file('contains_image_and_text_contains_word_image')
+        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('contains_image_and_text_contains_word_image_expected')
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_image_in_panel(self):
         test_string = self.read_test_file('image_in_panel')
+        self.assertTrue(ImageBlockProcessor(self.ext, self.md.parser).test(None, test_string), msg=''.format(test_string))
+
         converted_test_string = markdown.markdown(test_string, extensions=[Kordac()])
         expected_string = self.read_test_file('image_in_panel_expected')
         self.assertEqual(expected_string, converted_test_string)
