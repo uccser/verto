3b8900dd63b89b0336a004a81327a2456345e471
Hayley van Waas <hvw10@uclive.ac.nz>
2017-02-03 17:06:12 +1300

    Removed test statements

--- run.py
+++ run.py
@@ -24,21 +24,3 @@ class Kordac():
 
         return self
 
-hello = Kordac()
-something = hello.run('this is a test string')
-print(something.html_string)
-print(something.heading)
-
-        # for chapter in ['algorithms', 'introduction']:
-            # with open("{}.md".format(chapter), 'r') as f:
-                # s = f.read()
-
-                # html = converter.convert(s)
-
-            # with open("output/{}.html".format(chapter), 'w', encoding='utf8') as f:
-                # f.write(html)
-
-
-# if __name__ == "__main__":
-    # main()
-
