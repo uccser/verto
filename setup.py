66d720507aa7df7b01c5e97d69a367e51d1f5770
Jack Morgan <jackmorgannz@gmail.com>
2017-02-05 21:54:26 +1300

    Add setup.py and reorder folder structure for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='kordac',
+    version='0.0.1',
+    description='Kordac is an extension of the Python Markdown package, which allows authors to include complex HTML elements with simple text tags in their Markdown files.',
+    url='https://github.com/uccser/kordac',
+    author='UCCSER',
+    author_email='jack.morgan@canterbury.ac.nz',
+    license='MIT',
+    packages=['kordac'],
+    zip_safe=False
+)
