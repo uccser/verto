cb41ccbdd3d5b1761f06f623922b771ba5258662
Jack Morgan <jackmorgannz@gmail.com>
2017-02-06 16:05:30 +1300

    Return unique result object for each conversion (fixes #33)

--- kordac/Kordac.py
+++ kordac/Kordac.py
@@ -2,22 +2,35 @@ import markdown
 import mdx_math
 from kordac.KordacExtension import KordacExtension
 
-class Kordac():
+class Kordac(object):
+    """A converter object for converting markdown
+    to HTML"""
 
-    def run(self, md_string):
-        self.heading = 'I am a heading'
+    def run(self, markdown_string):
+        kordac_extension = KordacExtension()
+        kordac_extension.heading = None
         self.required_files = {}
-        self.html_string = ''
-        html = None
-        ext = KordacExtension()
+
         converter = markdown.Markdown(extensions=[
             'markdown.extensions.fenced_code',
             'markdown.extensions.codehilite',
             'markdown.extensions.sane_lists',
             mdx_math.MathExtension(enable_dollar_delimiter=True),
-            ext])
+            kordac_extension])
+        html = converter.convert(markdown_string)
 
-        self.html_string = converter.convert(md_string)
-        self.heading = ext.page_heading
+        result = KordacResult(
+            html=html,
+            heading=kordac_extension.page_heading
+        )
+        return result
 
-        return self
+
+class KordacResult(object):
+    """Object created by Kordac containing result of
+    a conversion by run
+    """
+
+    def __init__(self, html=None, heading=None):
+        self.html = html
+        self.heading = heading
