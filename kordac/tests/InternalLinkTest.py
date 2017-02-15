b3f10fabe2412ce331b74492b4a4e3214798ac5c
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 11:31:11 +1300

    Add ignore http schema test for internal link (#6)

--- kordac/tests/InternalLinkTest.py
+++ kordac/tests/InternalLinkTest.py
@@ -47,7 +47,14 @@ class InternalLinkTest(BaseTestCase):
         self.assertEqual(expected_string, converted_test_string)
 
     def test_ignore_http_schema(self):
-        pass
+        test_string = self.read_test_file('http_schema')
+
+        processor = InternalLinkPattern(self.ext, self.md.parser)
+        self.assertIsNone(re.search(processor.compiled_re, test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_expected_output_file('http_schema_expected').strip()
+        self.assertEqual(expected_string, converted_test_string)
 
     def test_http_text(self):
         pass
