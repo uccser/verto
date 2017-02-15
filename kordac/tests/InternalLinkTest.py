b6ceb3c543490911d0ec0e4a19d4f5d874e04277
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 11:35:05 +1300

    Add https schema test for internal link (#6)

--- kordac/tests/InternalLinkTest.py
+++ kordac/tests/InternalLinkTest.py
@@ -67,7 +67,14 @@ class InternalLinkTest(BaseTestCase):
         self.assertEqual(expected_string, converted_test_string)
 
     def test_ignore_https_schema(self):
-        pass
+        test_string = self.read_test_file('https_schema')
+
+        processor = InternalLinkPattern(self.ext, self.md.parser)
+        self.assertIsNone(re.search(processor.compiled_re, test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_expected_output_file('https_schema_expected').strip()
+        self.assertEqual(expected_string, converted_test_string)
 
     def test_https_text(self):
         pass
