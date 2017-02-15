79cae042a495cbccaf9b224fa4ccb989f3234651
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 11:45:47 +1300

    Add ignore ftps schema test for internal link (#6)

--- kordac/tests/InternalLinkTest.py
+++ kordac/tests/InternalLinkTest.py
@@ -107,7 +107,14 @@ class InternalLinkTest(BaseTestCase):
         self.assertEqual(expected_string, converted_test_string)
 
     def test_ignore_ftps_schema(self):
-        pass
+        test_string = self.read_test_file('ftps_schema')
+
+        processor = InternalLinkPattern(self.ext, self.md.parser)
+        self.assertIsNone(re.search(processor.compiled_re, test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_expected_output_file('ftps_schema_expected').strip()
+        self.assertEqual(expected_string, converted_test_string)
 
     def test_ftps_text(self):
         pass
