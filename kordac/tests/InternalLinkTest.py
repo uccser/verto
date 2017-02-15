073f1ae557b820639fdae98279d2c2040f0d16eb
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 11:46:43 +1300

    Add ftps text test for internal link (#6)

--- kordac/tests/InternalLinkTest.py
+++ kordac/tests/InternalLinkTest.py
@@ -117,7 +117,14 @@ class InternalLinkTest(BaseTestCase):
         self.assertEqual(expected_string, converted_test_string)
 
     def test_ftps_text(self):
-        pass
+        test_string = self.read_test_file('ftps_text')
+
+        processor = InternalLinkPattern(self.ext, self.md.parser)
+        self.assertIsNotNone(re.search(processor.compiled_re, test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_expected_output_file('ftps_text_expected').strip()
+        self.assertEqual(expected_string, converted_test_string)
 
     def test_ignore_mailto_schema(self):
         pass
