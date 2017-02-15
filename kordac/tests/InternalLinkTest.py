834947748cb38daf2eb93959ec3290c872300853
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 11:29:16 +1300

    Fix file paths for test case

--- kordac/tests/InternalLinkTest.py
+++ kordac/tests/InternalLinkTest.py
@@ -27,13 +27,13 @@ class InternalLinkTest(BaseTestCase):
         self.assertEqual(expected_string, converted_test_string)
 
     def test_long_path(self):
-        test_string = self.read_test_file('doc_example_basic_usage')
+        test_string = self.read_test_file('long_path')
 
         processor = InternalLinkPattern(self.ext, self.md.parser)
         self.assertIsNotNone(re.search(processor.compiled_re, test_string))
 
         converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
-        expected_string = self.read_expected_output_file('doc_example_basic_usage_expected').strip()
+        expected_string = self.read_expected_output_file('long_path_expected').strip()
         self.assertEqual(expected_string, converted_test_string)
 
     def test_multiple_links(self):
