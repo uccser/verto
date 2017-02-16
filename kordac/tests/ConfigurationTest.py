c32c701243ed168ec3a0927a8a1e004beefba965
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 21:05:56 +1300

    Add unique custom processors test

--- kordac/tests/ConfigurationTest.py
+++ kordac/tests/ConfigurationTest.py
@@ -18,16 +18,27 @@ class ConfigurationTest(unittest.TestCase):
     def test_default_processors_on_creation(self):
         kordac = Kordac()
         default_processors = kordac.processor_defaults()
-        self.assertTrue(kordac.kordac_extension.processors, default_processors)
+        self.assertEqual(kordac.kordac_extension.processors, default_processors)
 
     def test_custom_processors_on_creation(self):
         processors = {'comment', 'image'}
         kordac = Kordac(processors=processors)
-        self.assertTrue(kordac.kordac_extension.processors, processors)
+        self.assertEqual(kordac.kordac_extension.processors, processors)
 
     def test_custom_processors_after_creation(self):
         kordac = Kordac()
         processors = kordac.processor_defaults()
         processors.add('example_processor')
+        kordac.update_processors(processors)
+        self.assertEqual(kordac.kordac_extension.processors, processors)
+
+    def test_unique_custom_processors(self):
+        processors = ['comment', 'comment', 'comment']
+        kordac = Kordac(processors=processors)
+        self.assertEqual(kordac.kordac_extension.processors, set(processors))
+        processors = list(kordac.processor_defaults())
+        processors.append('example_processor')
+        processors.append('example_processor')
+        processors.append('example_processor')
         kordac.update_processors(processors)
         self.assertTrue(kordac.kordac_extension.processors, processors)
