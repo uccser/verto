54b2a9e076b9ec6b9b32e497fda8c1e5681f9a24
Jack Morgan <jackmorgannz@gmail.com>
2017-02-16 22:29:40 +1300

    Add reset templates after custom test

--- kordac/tests/ConfigurationTest.py
+++ kordac/tests/ConfigurationTest.py
@@ -84,3 +84,29 @@ class ConfigurationTest(unittest.TestCase):
             stored_template = kordac.kordac_extension.jinja_templates[processor_name]
             rendered_stored_template = stored_template.render()
             self.assertEqual(rendered_expected_template, rendered_stored_template)
+
+    def test_reset_templates_after_custom(self):
+        # This test doesn't do a perfect check that both templates are the same
+        # as we cannot access the raw string stored within the loaded template
+        # Instead we check if the rendered version of both is the same, however
+        # this doesn't check context variables are the same.
+        default_template_path = 'kordac/html-templates/{}.html'
+        custom_templates = {
+            'image': '<img />',
+            'boxed-text': '<div class="box"></div>'
+        }
+        kordac = Kordac(html_templates=custom_templates)
+        for processor_name, template in custom_templates.items():
+            expected_template = jinja2.Template(template)
+            rendered_expected_template = expected_template.render()
+            stored_template = kordac.kordac_extension.jinja_templates[processor_name]
+            rendered_stored_template = stored_template.render()
+            self.assertEqual(rendered_expected_template, rendered_stored_template)
+        kordac.default_templates()
+        for processor_name in custom_templates.keys():
+            default_template = open(default_template_path.format(processor_name), 'r').read()
+            expected_template = jinja2.Template(default_template)
+            rendered_expected_template = expected_template.render()
+            stored_template = kordac.kordac_extension.jinja_templates[processor_name]
+            rendered_stored_template = stored_template.render()
+            self.assertEqual(rendered_expected_template, rendered_stored_template)
