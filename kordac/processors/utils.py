1863c4774750ba773865d6a547c5943d6096ff47
Hayden Jackson <hayden.adam.jackson@gmail.com>
2017-02-17 15:06:26 +1300

    trash zombie code.

--- kordac/processors/utils.py
+++ kordac/processors/utils.py
@@ -1,13 +1,6 @@
 import re
 from markdown.util import etree
 
-CENTERED_HTML = """
-<div class='row'>
-  <div content='' class='content col s12 m{width} offset-m{offset_width} center-align'>
-  </div>
-</div>
-"""
-
 def parse_argument(argument_key, arguments, default=None, convert_type=True):
     """Search for the given argument in a string of all arguments
     Returns: Value of an argument as a string if found, otherwise None"""
@@ -31,21 +24,6 @@ def string_to_type(string):
     except ValueError:
         pass
     return string
-
-def from_kebab_case(text):
-    """Returns given kebab case text to plain text.
-    Text is camel case, with dashs replaced with spaces
-    """
-    return text.replace('-', ' ').title()
-
-def to_kebab_case(text):
-    """Returns the given text as kebab case.
-    The text is lower case, has spaces replaced as dashes.
-    All punctuation is also removed.
-    """
-    text = ''.join(letter for letter in text if letter in set(string.ascii_letters + string.digits + ' -'))
-    text = text.replace(' ', '-').lower()
-    return text
 
 def blocks_to_string(blocks):
     """Returns a string after the blocks have been joined back together."""
