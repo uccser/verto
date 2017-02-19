c322e2504f38d39872cdea39ea7817feb80bdaef
ravenmaster001 <hayden.adam.jackson@gmail.com>
2017-02-20 12:27:53 +1300

    Update button test for a single button present.

--- kordac/tests/ButtonLinkTest.py
+++ kordac/tests/ButtonLinkTest.py
@@ -21,21 +21,20 @@ class ButtonLinkTest(ProcessorTest):
         test_string = self.read_test_file(self.processor_name, 'no_button.md')
         blocks = self.to_blocks(test_string)
 
-        self.assertFalse(any(ButtonLinkBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks), msg='"{}"'.format(test_string))
+        self.assertListEqual([False] * 7, [ButtonLinkBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
         converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
         expected_string = self.read_test_file(self.processor_name, 'no_button_expected.html', strip=True)
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_contains_button(self):
+        test_string = self.read_test_file(self.processor_name, 'contains_button.md')
+        blocks = self.to_blocks(test_string)
 
-    # def test_contains_button(self):
-    #     test_string = self.read_test_file(self.processor_name, 'contains_button.md')
-    #     blocks = self.to_blocks(test_string)
-    #
-    #     self.assertTrue(all(ButtonLinkBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks), msg='"{}"'.format(test_string))
-    #     expected_string = self.read_test_file(self.processor_name, 'contains_button_expected.html', strip=True)
-    #     converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
-    #     expected_string = self.read_expected_output_file('contains_button_expected')
-    #     self.assertEqual(expected_string, converted_test_string)
+        self.assertListEqual([False, True, False], [ButtonLinkBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_test_file(self.processor_name, 'contains_button_expected.html', strip=True)
+        self.assertEqual(expected_string, converted_test_string)
     #
     # def test_contains_button(self):
     #     test_string = self.read_test_file('missing_end_brace')
