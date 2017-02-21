d737184c36915c881bc8599373607e70b885da1f
Hayley van Waas <hayleyavw@gmail.com>
2017-02-21 15:13:14 +1300

    Added testing for docs to video tag

--- kordac/tests/VideoTest.py
+++ kordac/tests/VideoTest.py
@@ -111,3 +111,30 @@ class VideoTest(ProcessorTest):
         expected_file_string = self.read_test_file(self.processor_name, 'youtube_and_vimeo_links_expected.html', strip=True)
         self.assertEqual(converted_test_string, expected_file_string)
 
+    #~
+    # Doc Tests
+    #~
+
+    def test_doc_example_basic(self):
+        test_string = self.read_test_file(self.processor_name, 'doc_example_basic_usage.md')
+        blocks = self.to_blocks(test_string)
+
+        self.assertListEqual([True], [VideoBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_test_file(self.processor_name, 'doc_example_basic_usage_expected.html', strip=True)
+        self.assertEqual(expected_string, converted_test_string)
+
+    def test_doc_example_override_html(self):
+        test_string = self.read_test_file(self.processor_name, 'doc_example_override_html.md')
+        blocks = self.to_blocks(test_string)
+
+        self.assertListEqual([True], [VideoBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
+
+        html_template = self.read_test_file(self.processor_name, 'doc_example_override_html_template.html', strip=True)
+        kordac_extension = KordacExtension([self.processor_name], html_templates={self.processor_name: html_template})
+
+        converted_test_string = markdown.markdown(test_string, extensions=[kordac_extension])
+        expected_string = self.read_test_file(self.processor_name, 'doc_example_override_html_expected.html', strip=True)
+        self.assertEqual(expected_string, converted_test_string)
+
