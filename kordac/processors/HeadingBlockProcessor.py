fc74a5ba80526dcca41c34d99fccee22b9914cbb
ravenmaster001 <hayden.adam.jackson@gmail.com>
2017-03-01 14:46:09 +1300

    Implemented heading processor.

---
+++ kordac/processors/HeadingBlockProcessor.py
@@ -0,0 +1,103 @@
+from markdown.blockprocessors import BlockProcessor
+from markdown.util import etree
+from slugify import UniqueSlugify
+import re
+
+class HeadingBlockProcessor(BlockProcessor):
+    ''' Searches a Document for markdown headings (e.g. # HeadingTitle) these are then processed into html headings and generates level trails for each.
+    '''
+
+    def __init__(self, ext, *args, **kwargs):
+        '''
+        Args:
+            ext: The parent node of the element tree that children will reside in.
+            args: Arguments handed to the super class.
+            kwargs: Arguments handed to the super class.
+        '''
+        super().__init__(*args, **kwargs)
+        self.processor = 'heading'
+        self.max_levels = 6
+        self.pattern = re.compile(ext.processor_info[self.processor]['pattern'])
+        self.template = ext.jinja_templates[self.processor]
+        self.custom_slugify = UniqueSlugify()
+        self.level_gen = LevelGenerator(self.max_levels)
+
+    def test(self, parent, block):
+        ''' Tests a block to see if the run method should be applied.
+
+        Args:
+            parent: The parent node of the element tree that children will reside in.
+            block: The block to be tested.
+
+        Returns:
+            True if the block matches the pattern regex of a HeadingBlock.
+        '''
+        return self.pattern.search(block) is not None
+
+    def run(self, parent, blocks):
+        ''' Increments the level trail at the given level. This also
+        zeros any levels after the incremented level.
+
+        Args:
+            level_inc: the level to be incremented.
+
+        Returns:
+            A tuple of levels, where higher levels are first and the lowest
+            level is last.
+        '''
+        block = blocks.pop(0)
+        match = self.pattern.search(block)
+        assert match is not None # If this is true how did we test successfully
+
+        before = block[:match.start()]
+        after = block[match.end():]
+
+        if before:
+            self.parser.parseBlocks(parent, [before])
+        if after:
+            blocks.insert(0, after)
+
+        level = len(match.group('level'))
+        heading = match.group('header').strip()
+        level_trail = self.number_generator.next(level)
+
+        context = dict()
+        context['heading_level'] = level
+        context['title'] = heading
+        context['title_slug'] = custom_slugify(heading)
+        context.update(
+            zip(("level_{0}".format(level) for level in range(1, self.max_levels + 1))
+                , level_trail))
+
+        html_string = self.template.render(context)
+        node = etree.fromstring(html_string)
+        parent.append(node)
+
+class LevelGenerator:
+    ''' Generates a level trail for example (1, 2, 3) which might be
+    interpretted as chapter 1, section 2 and subsection 3.
+    '''
+
+    def __init__(self, max_levels):
+        '''
+        Args:
+            max_levels: The max number of levels for the generator to handle.
+        '''
+        self.level_list = [0 for _ in range(max_levels)]
+
+    def next(self, level_inc):
+        ''' Increments the level trail at the given level. This also
+        zeros any levels after the incremented level.
+
+        Args:
+            level_inc: the level to be incremented.
+
+        Returns:
+            A tuple of levels, where higher levels are first and the lowest
+            level is last.
+        '''
+        assert level_inc-1 < len(level_list)
+        self.level_list[level_inc-1] += 1
+        for index in range(level_inc, len(level_list)):
+            level_inc[index] = 0
+        return tuple(self.level_list)
