e3dfea8c97c5e51d89fc8114f5d9422d98d31559
ravenmaster001 <hayden.adam.jackson@gmail.com>
2017-03-03 09:19:09 +1300

    Added some simple tests.

--- kordac/tests/HeadingTest.py
+++ kordac/tests/HeadingTest.py
@@ -16,6 +16,40 @@ class HeadingTest(ProcessorTest):
         self.ext.processor_info = ProcessorTest.loadProcessorInfo(self)
         self.ext.jinja_templates = {self.processor_name: ProcessorTest.loadJinjaTemplate(self, self.processor_name)}
 
+    def test_example_blank(self):
+        test_string = self.read_test_file(self.processor_name, 'example_blank.md')
+        blocks = self.to_blocks(test_string)
+
+        self.assertListEqual([False], [HeadingBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_test_file(self.processor_name, 'example_blank_expected.html', strip=True)
+        self.assertEqual(expected_string, converted_test_string)
+
+        tree = self.kordac_extension.get_heading_tree()
+        expected_tree = None
+        self.assertEqual(tree, expected_tree)
+
+    def test_single_heading(self):
+        test_string = self.read_test_file(self.processor_name, 'example_single_heading.md')
+        blocks = self.to_blocks(test_string)
+
+        self.assertListEqual([True], [HeadingBlockProcessor(self.ext, self.md.parser).test(blocks, block) for block in blocks], msg='"{}"'.format(test_string))
+
+        converted_test_string = markdown.markdown(test_string, extensions=[self.kordac_extension])
+        expected_string = self.read_test_file(self.processor_name, 'example_single_heading_expected.html', strip=True)
+        self.assertEqual(expected_string, converted_test_string)
+
+        tree = self.kordac_extension.get_heading_tree()
+        expected_tree = (
+            HeadingNode(title='Heading One',
+                                        title_slug='heading-one',
+                                        level=1,
+                                        children=()
+            ),
+        )
+        self.assertEqual(tree, expected_tree)
+
     #~
     # Doc Tests
     #~
