34de398c0d8150de7ed54bc991e69f0ea526636d
ravenmaster001 <hayden.adam.jackson@gmail.com>
2017-03-10 15:41:34 +1300

    Added comments/docstrings based on PR.

--- kordac/utils/overrides.py
+++ kordac/utils/overrides.py
@@ -6,9 +6,19 @@ BLOCK_LEVEL_ELEMENTS = [
     'dl', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h[1-6]',
     'header', 'hr', 'li', 'main', 'nav', 'noscript', 'ol', 'output', 'p',
     'pre', 'section', 'table', 'tfoot', 'ul', 'video', 'remove'
-] # TO MAKE CONFIGURABLE
+]  # TODO: TO MAKE CONFIGURABLE
 
 def is_block_level(html, block_level_elements):
+    '''
+    Checks if the root element (or first element) of the given
+    html is a block level element.
+    Args:
+        html: A string of the html to check.
+        block_level_elements: A list of strings which are the
+        block level elements.
+    Returns:
+        True if the first element is a block level element.
+    '''
     m = re.match(r'^\<\/?([^ >]+)', html)
     if m:
         tag = m.group(1)
