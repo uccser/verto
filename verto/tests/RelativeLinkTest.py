a3397639bde5a5df74f4cd6cb9318fe9d194e0d7
Jack Morgan <jackmorgannz@gmail.com>
2017-12-27 19:43:15 +1300

    Modify relative link to separate link from query parameters #255

--- verto/tests/RelativeLinkTest.py
+++ verto/tests/RelativeLinkTest.py
@@ -43,6 +43,36 @@ class RelativeLinkTest(ProcessorTest):
         expected_string = self.read_test_file(self.processor_name, 'long_path_expected.html', strip=True).strip()
         self.assertEqual(expected_string, converted_test_string)
 
+    def test_query_parameter(self):
+        '''Tests that paths with query parameter work.
+        '''
+        test_string = self.read_test_file(self.processor_name, 'query_parameter.md')
+        processor = RelativeLinkPattern(self.ext, self.md.parser)
+        self.assertIsNotNone(re.search(processor.compiled_re, test_string))
+        converted_test_string = markdown.markdown(test_string, extensions=[self.verto_extension])
+        expected_string = self.read_test_file(self.processor_name, 'query_parameter_expected.html', strip=True).strip()
+        self.assertEqual(expected_string, converted_test_string)
+
+    def test_multiple_query_parameters(self):
+        '''Tests that paths with multiple query parameters work.
+        '''
+        test_string = self.read_test_file(self.processor_name, 'multiple_query_parameters.md')
+        processor = RelativeLinkPattern(self.ext, self.md.parser)
+        self.assertIsNotNone(re.search(processor.compiled_re, test_string))
+        converted_test_string = markdown.markdown(test_string, extensions=[self.verto_extension])
+        expected_string = self.read_test_file(self.processor_name, 'multiple_query_parameters_expected.html', strip=True).strip()
+        self.assertEqual(expected_string, converted_test_string)
+
+    def test_trailing_question_mark(self):
+        '''Tests that paths with trailing question marks.
+        '''
+        test_string = self.read_test_file(self.processor_name, 'trailing_question_mark.md')
+        processor = RelativeLinkPattern(self.ext, self.md.parser)
+        self.assertIsNotNone(re.search(processor.compiled_re, test_string))
+        converted_test_string = markdown.markdown(test_string, extensions=[self.verto_extension])
+        expected_string = self.read_test_file(self.processor_name, 'trailing_question_mark_expected.html', strip=True).strip()
+        self.assertEqual(expected_string, converted_test_string)
+
     def test_multiple_links(self):
         '''Tests that multiple links are processed.
         '''
