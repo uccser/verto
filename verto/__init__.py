0a48c6b9d8dad21dd149bacd8fc5ed55eb391620
Philip Dolbel <pdo66@uclive.ac.nz>
2023-01-18 09:37:43 +1300

    Add comments explaining test pypi versioning

--- verto/__init__.py
+++ verto/__init__.py
@@ -1,4 +1,7 @@
 # flake8: noqa
 from .Verto import Verto
 
+# This must be the last line of the file
+# as publish-to-pypi.py appends to it
+# to update the version number for Test PyPI.
 __version__ = '1.1.1'
