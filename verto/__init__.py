e230cddbf7312d5837243afbd6995758f067332a
Yamgirl <philip.dolbel@canterbury.ac.nz>
2026-03-18 13:39:02 +1300

    Update version to 1.2.0

--- verto/__init__.py
+++ verto/__init__.py
@@ -4,4 +4,4 @@ from .Verto import Verto
 # This must be the last line of the file
 # as publish-to-pypi.py appends to it
 # to update the version number for Test PyPI.
-__version__ = '1.1.1'
+__version__ = '1.2.0'
